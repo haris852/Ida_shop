3b8a920a965da05aa28a122648ff445de2ce2e8b
MOH IBNU ABDURROHMAN SUTIO <ibnuabdurrohmansutio@gmail.com>
2023-05-15 08:53:10 +0700

    commit #2
    Action:
    add update profile

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SettingController extends Controller
 {
@@ -52,7 +54,45 @@ class SettingController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'sex' => ['required'],
+            'phone' => ['required', 'string', 'max:255'],
+            'address' => ['required'],
+            'email' => ['nullable', 'string', 'email', 'max:255'],
+            'password' => ['nullable', 'string', 'min:8', 'confirmed'],
+            'avatar' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048']
+        ]);
+
+
+        try {
+            $user = User::find($id);
+            if (isset($request->avatar)) {
+                $oldAvatar = $user->avatar;
+                if ($oldAvatar != null) {
+                    Storage::delete('public/avatar/' . $oldAvatar);
+                }
+                $filename = uniqid() . $request->avatar->getClientOriginalName();
+                $request->avatar->storeAs('public/avatar', $filename);
+
+                $user->avatar = $filename;
+            }
+
+            $user->name = $request->name;
+            $user->sex = $request->sex;
+            $user->phone = $request->phone;
+            $user->address = $request->address;
+            $user->phone = $request->phone;
+            $user->email = $request->email;
+            if (isset($request->password)) {
+                $user->password = password_hash($request->password, PASSWORD_DEFAULT);
+            }
+            $user->save();
+
+            return redirect()->route('admin.setting.index')->with('success', 'Profil pengguna berhasil diperbarui!');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
     }
 
     /**
