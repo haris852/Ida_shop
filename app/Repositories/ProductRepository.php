a51b48b8d750cd2ec5513bf5fbae201b1577cceb
MOH IBNU ABDURROHMAN SUTIO <ibnuabdurrohmansutio@gmail.com>
2023-05-19 08:48:28 +0700

    update error in product

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -67,7 +67,7 @@ class ProductRepository implements ProductInterface
 
             return true;
         } catch (\Throwable $th) {
-            return false;
+            throw $th;
         }
     }
 }
