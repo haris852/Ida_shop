a6a406c06df68b4b0bea1d4755126b2983f6d2b6
MOH IBNU ABDURROHMAN SUTIO <ibnuabdurrohmansutio@gmail.com>
2023-05-26 19:56:49 +0700

    Update edit.blade.php

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -76,7 +76,7 @@
                                         required />
                                     <div class="form-group">
                                         <label for="description">Deskripsi</label>
-                                        <textarea class="form-control" id="description" name="description" rows="4"></textarea>
+                                        <textarea class="form-control" id="description" name="description" rows="4">{{ $data->description }}</textarea>
                                     </div>
                                 </div>
                             </div>
