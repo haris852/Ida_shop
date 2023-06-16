2161895dc893718cecedf885af8871cf8d651fbf
MOH IBNU ABDURROHMAN SUTIO <ibnuabdurrohmansutio@gmail.com>
2023-06-16 17:48:21 +0700

    update status order label

--- resources/views/admin/order/column/status.blade.php
+++ resources/views/admin/order/column/status.blade.php
@@ -1,7 +1,12 @@
 @if ($data->status == 'pending')
+    <!-- expired -->
+    @if ($data->created_at->addMinutes(60) < now())
+        <button class="btn btn-rounded btn-danger btn-sm text-white">Pesanan Kadaluarsa</button>
+    @else
     <button class="btn btn-rounded btn-dark btn-sm text-white">
         {{$data->payment_method == 1 ? 'Menunggu Pembayaran' : 'Menunggu Penerimaan'}}
     </button>
+    @endif
 @elseif ($data->status == 'paid')
     <button class="btn btn-rounded btn-warning btn-sm">Pesanan Dibayar</button>
 @elseif ($data->status == 'confirmed')
